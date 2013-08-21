a8617562d2ff6afb16cff2ace248f543f5f81835
Matthias Kuhn <matthias.kuhn@gmx.ch>
2013-08-21 16:19:40 +0200

    Fix globe python bindings

--- src/plugins/globe/python/__init__.py
+++ src/plugins/globe/python/__init__.py
@@ -4,9 +4,9 @@
 ***************************************************************************
     __init__.py
     ---------------------
-    Date                 : January 2007
-    Copyright            : (C) 2007 by Martin Dobias
-    Email                : wonder dot sk at gmail dot com
+    Date                 : Aug 2013
+    Copyright            : (C) 2013 by Matthias Kuhn
+    Email                : matthias dot kuhn at gmx dot ch
 ***************************************************************************
 *                                                                         *
 *   This program is free software; you can redistribute it and/or modify  *
@@ -17,32 +17,8 @@
 ***************************************************************************
 """
 
-__author__ = 'Martin Dobias'
-__date__ = 'January 2007'
-__copyright__ = '(C) 2007, Martin Dobias'
+__author__ = 'Matthias Kuhn'
+__date__ = 'August 2013'
+__copyright__ = '(C) 2013, Matthias Kuhn'
 # This will get replaced with a git SHA1 when you do a git archive
 __revision__ = '$Format:%H$'
-
-import sip
-
-try:
-    apis = ["QDate", "QDateTime", "QString", "QTextStream", "QTime", "QUrl", "QVariant"]
-    for api in apis:
-        sip.setapi(api, 2)
-except ValueError:
-    # API has already been set so we can't set it again.
-    pass
-
-try:
-    # Add a __nonzero__ method onto QPyNullVariant so we can check for null values easier.
-    #   >>> value = QPyNullVariant("int")
-    #   >>> if value:
-    #   >>>	  print "Not a null value"
-    from types import MethodType
-    from PyQt4.QtCore import QPyNullVariant
-    def __nonzero__(self):
-        return False
-
-    QPyNullVariant.__nonzero__ = MethodType(__nonzero__, None, QPyNullVariant)
-except ImportError:
-    pass
