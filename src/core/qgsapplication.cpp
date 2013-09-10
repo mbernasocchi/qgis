3a755301ba175711fae47268eae73ef28bd6ee64
Marco Bernasocchi <marco@opengis.ch>
2013-09-10 15:10:39 +0200

    added ui_defaults_android.h for storing android specific ui defaults
    typo in comments

--- src/core/qgsapplication.cpp
+++ src/core/qgsapplication.cpp
@@ -140,11 +140,11 @@ void QgsApplication::init( QString customConfigPath )
 #if defined(Q_WS_MACX) || defined(Q_WS_WIN32) || defined(WIN32)
       setPrefixPath( applicationDirPath(), true );
 #elif defined(ANDROID)
-      // this is  "/data/data/org.qgis.qgis" in android
-      QDir myDir( QDir::homePath() );
-      myDir.cdUp();
-      QString myPrefix = myDir.absolutePath();
-      setPrefixPath( myPrefix, true );
+    // this is  "/data/data/org.qgis.qgis/files" in android
+    QDir myDir( QDir::homePath() );
+    myDir.cdUp();
+    QString myPrefix = myDir.absolutePath();
+    setPrefixPath( myPrefix, true );
 #else
       QDir myDir( applicationDirPath() );
       myDir.cdUp();
@@ -178,6 +178,25 @@ void QgsApplication::init( QString customConfigPath )
   }
   ABISYM( mSystemEnvVars ) = systemEnvVarMap;
 
+  // set a working directory up for gdal to write .aux.xml files into
+  // for cases where the raster dir is read only to the user
+  // if the env var is already set it will be used preferentially
+  QString myPamPath = qgisSettingsDirPath() + QString( "gdal_pam/" );
+  QDir myDir( myPamPath );
+  if ( !myDir.exists() )
+  {
+    myDir.mkpath( myPamPath ); //fail silently
+  }
+
+
+#if defined(Q_WS_WIN32) || defined(WIN32)
+  CPLSetConfigOption( "GDAL_PAM_PROXY_DIR", myPamPath.toUtf8() );
+#else
+  //under other OS's we use an environment var so the user can
+  //override the path if he likes
+  int myChangeFlag = 0; //whether we want to force the env var to change
+  setenv( "GDAL_PAM_PROXY_DIR", myPamPath.toUtf8(), myChangeFlag );
+#endif
 }
 
 QgsApplication::~QgsApplication()
@@ -881,27 +900,8 @@ void QgsApplication::applyGdalSkippedDrivers()
   GDALAllRegister(); //to update driver list and skip missing ones
 }
 
-bool QgsApplication::createDB( QString *errorMessage )
+bool QgsApplication::createDB( QString* errorMessage )
 {
-  // set a working directory up for gdal to write .aux.xml files into
-  // for cases where the raster dir is read only to the user
-  // if the env var is already set it will be used preferentially
-  QString myPamPath = qgisSettingsDirPath() + QString( "gdal_pam/" );
-  QDir myDir( myPamPath );
-  if ( !myDir.exists() )
-  {
-    myDir.mkpath( myPamPath ); //fail silently
-  }
-
-#if defined(Q_WS_WIN32) || defined(WIN32)
-  CPLSetConfigOption( "GDAL_PAM_PROXY_DIR", myPamPath.toUtf8() );
-#else
-  //under other OS's we use an environment var so the user can
-  //override the path if he likes
-  int myChangeFlag = 0; //whether we want to force the env var to change
-  setenv( "GDAL_PAM_PROXY_DIR", myPamPath.toUtf8(), myChangeFlag );
-#endif
-
   // Check qgis.db and make private copy if necessary
   QFile qgisPrivateDbFile( QgsApplication::qgisUserDbFilePath() );
 
