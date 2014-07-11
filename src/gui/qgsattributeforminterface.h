fd7b292011fb06feac040b84574e22aaed047e1f
Matthias Kuhn <matthias.kuhn@gmx.ch>
2014-07-11 23:40:40 +0200

    Fix warning

--- src/gui/qgsattributeforminterface.h
+++ src/gui/qgsattributeforminterface.h
@@ -23,6 +23,7 @@ class GUI_EXPORT QgsAttributeFormInterface
 {
   public:
     explicit QgsAttributeFormInterface( QgsAttributeForm* form );
+    virtual ~QgsAttributeFormInterface() {}
 
     virtual bool acceptChanges( const QgsFeature& feature );
 
